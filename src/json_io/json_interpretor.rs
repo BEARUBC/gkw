0bc89b2622672e67c7bc9097211f26ad9a37ff5d
Maharshi Panchal <panchalmaharshi8@gmail.com>
2021-02-20 15:41:45 -0700

    testing

--- src/json_io/json_interpretor.rs
+++ src/json_io/json_interpretor.rs
@@ -2,43 +2,10 @@ use serde_json::json;
 // use crate::read_from_file;
 use crate::json_io_0::file_io::*;
 
-<<<<<<< HEAD
 use super::file_io::write_to_file;
 use serde_json::{Result, Value};
 
-pub fn deserial_json() -> Result<()>{
-    let json_object = read_from_file("input.json");
-    
-    let j: Value = serde_json::from_str(&json_object)?;
 
-    write_to_file(&j.to_string(), "output.json");
-
-    Ok(())
-}
-
-// pub fn testJson() {
-//     let x = deserial_json();
-    
-//     write_to_file(&x.to_string(), "output.json")
-// }
-=======
-// use super::file_io::write_to_file;
->>>>>>> piiobranch_fixes
-
-pub fn instance() {
-    let ex = json!({
-        "x": 34,
-        "y": "a string",
-        "z": [3,4,5],
-    });
-
-    let json_object = json!(read_from_file("input.json"));
-
-    let j = serde_json::to_string(&json_object);
-
-    //println!("{}", test.to_string());
-    //println!("{}", ex.to_string());
-}
 
 pub fn write_json(filename: &str){
     let ex = json!({
--- src/json_io_0.rs
+++ src/json_io_0.rs
@@ -32,8 +32,57 @@ pub fn write_to_input(content: &[u8]) -> std::io::Result<()> {
 
 #[cfg(test)]
 mod json_io_test {
+    use serde_json::{Result, Value, json};
+    use super::write_to_input;
+
     #[test]
-    fn it_works() -> () {
-        assert_eq!(2u8, 2u8);
+    fn simple_write() -> () {
+        let ex = json!({
+            "x": 34,
+            "y": "a string",
+            "z": [3,4,5],
+        });
+
+        let j = ex.to_string();
+
+        let x = j.as_bytes();
+
+        let result = write_to_input(x);
+
+        match result{
+            Ok(()) => {},
+            Err(e) => {panic!();},
+        }
     }
+
+    // pub fn deserial_json() -> Result<()>{
+    //     let json_object = read_from_file("input.json");
+        
+    //     let j: Value = serde_json::from_str(&json_object)?;
+    
+    //     write_to_file(&j.to_string(), "output.json");
+    
+    //     Ok(())
+    // }
+    
+    // pub fn testJson() {
+    //     let x = deserial_json();
+        
+    //     write_to_file(&x.to_string(), "output.json")
+    // }
+    
+    // pub fn instance() {
+    //     let ex = json!({
+    //         "x": 34,
+    //         "y": "a string",
+    //         "z": [3,4,5],
+    //     });
+    
+    //     let json_object = json!(read_from_file("input.json"));
+    
+    //     let j = serde_json::to_string(&json_object);
+    
+    //     //println!("{}", test.to_string());
+    //     //println!("{}", ex.to_string());
+    // }
 }
