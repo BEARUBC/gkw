28a0f5d0686c5b2399cbc62eee994f46883956f1
raunakab <rabhagat31@gmail.com>
2021-02-20 16:59:30 -0700

    finished reading from output

--- src/json_io.rs
+++ src/json_io.rs
@@ -1,16 +1,29 @@
 use std::{
     fs,
     fs::File,
+    io::{
+        prelude::*,
+        ErrorKind
+    },
 };
-use std::io::prelude::*;
+use serde_json::{
+    Result,
+    Value,
+    json
+};
 
-pub fn read_from_output() -> std::io::Result<String> {
+pub fn read_from_output() -> std::io::Result<Value> {
     let mut string_buffer = String::new();
 
     match File::open(format!("./py_io/output.json")) {
         Ok(mut file) => {
             match file.read_to_string(&mut string_buffer) {
-                Ok(_) => { return Ok(string_buffer); },
+                Ok(_) => {
+                    match serde_json::from_str(string_buffer.as_str()) {
+                        Ok(value) => { return Ok(value); },
+                        Err(err) => { return Err(std::io::Error::new(ErrorKind::Other, "unable to construct json from file")); },
+                    }
+                },
                 Err(err) => { return Err(err); },
             };
         },
@@ -18,10 +31,13 @@ pub fn read_from_output() -> std::io::Result<String> {
     };
 }
 
-pub fn write_to_input(content: &[u8]) -> std::io::Result<()> {
+pub fn write_to_input(json: Value) -> std::io::Result<()> {
+    let json_string = json.to_string();
+    let json_bytes = json_string.as_bytes();
+
     match File::create("./py_io/input.json") {
         Ok(mut file) => {
-            match file.write_all(content) {
+            match file.write_all(json_bytes) {
                 Ok(()) => { return Ok(()); },
                 Err(err) => { return Err(err); }
             }
@@ -33,54 +49,46 @@ pub fn write_to_input(content: &[u8]) -> std::io::Result<()> {
 #[cfg(test)]
 mod json_io_test {
     use serde_json::{Result, Value, json};
-    use super::write_to_input;
+    use super::*;
 
     #[test]
     fn simple_write() -> () {
-        let ex = json!({
+        let json = json!({
             "x": 34,
             "y": "a string",
             "z": [3,4,5],
         });
 
-        let j = ex.to_string();
-        let x = j.as_bytes();
-        let result = write_to_input(x);
+        let result = write_to_input(json);
 
-        match result{
+        match result {
             Ok(()) => (),
             Err(e) => { panic!(); },
         }
     }
 
-    // pub fn deserial_json() -> Result<()>{
-    //     let json_object = read_from_file("input.json");
-        
-    //     let j: Value = serde_json::from_str(&json_object)?;
-    
-    //     write_to_file(&j.to_string(), "output.json");
-    
-    //     Ok(())
-    // }
-    
-    // pub fn testJson() {
-    //     let x = deserial_json();
-        
-    //     write_to_file(&x.to_string(), "output.json")
-    // }
-    
-    // pub fn instance() {
-    //     let ex = json!({
-    //         "x": 34,
-    //         "y": "a string",
-    //         "z": [3,4,5],
-    //     });
-    
-    //     let json_object = json!(read_from_file("input.json"));
-    
-    //     let j = serde_json::to_string(&json_object);
-    
-    //     //println!("{}", test.to_string());
-    //     //println!("{}", ex.to_string());
-    // }
+    #[test]
+    fn complex_write() -> () {
+        let json = json!({
+            "x": 34,
+            "obj": {
+                "asdf": true,
+            },
+        });
+
+        let result = write_to_input(json);
+
+        match result {
+            Ok(()) => (),
+            Err(e) => { panic!(); },
+        }
+    }
+
+    #[test]
+    fn simple_read() -> () {
+        match read_from_output() {
+            Ok(json) => (),
+            Err(err) => { panic!(); }
+        };
+    }
 }
