02dc2f2468a7e73eaa79509bf77cba5876badf24
Raunak Bhagat <rabhagat31@gmail.com>
2021-05-24 13:16:33 -0600

    example `building a component` is working

--- async_framework/examples/building_a_component.rs
+++ async_framework/examples/building_a_component.rs
@@ -1,12 +1,7 @@
-use std::{
-    future::Future,
-    pin::Pin,
-    sync::Arc,
-    task::{
+use std::{future::Future, pin::Pin, task::{
         Context,
         Poll
-    }
-};
+    }};
 
 use tokio::runtime::Builder as TokioBuilder;
 
@@ -14,63 +9,69 @@ use async_framework::{
     component_builder::builder::ComponentBuilder,
     job::Job,
     routine_builder::builder::RoutineBuilder,
+    contacts::contacts::Contacts,
     builder::Builder,
 };
 
 struct MS;
 
 impl Future for MS {
-    type Output = ();
+    type Output = u32;
 
     fn poll(self: Pin<&mut Self>, _: &mut Context<'_>) -> Poll<Self::Output> {
-        println!("MS poll method called");
-        Poll::Pending
+        println!("handler for the MS");
+
+        Poll::Ready(0u32)
     }
 }
 
-async fn handler(message: MS) -> u32 {
-    message.await;
-    0u32
+async fn handler(_: Contacts<MS>, message: MS) -> u32 {
+    tokio::time::sleep(std::time::Duration::from_secs(10u64)).await;
+
+    let result = message.await;
+    println!("result: {}", result);
+
+    result
 }
 
-async fn lambda_1() -> u32 { println!("Hello, world, from lambda!"); 0u32 }
+async fn lambda_1(_: Contacts<MS>) -> u32 {
+    println!("Hello, world, from lambda!");
+    0u32
+}
 
 /* above is equivalent to:
 fn lambda_1() -> F
 where
 F: 
- */
+*/
 
 fn main() -> () {
     use std::env;
 
     let key = "RUST_BACKTRACE";
     env::set_var(key, "1");
-    // assert_eq!(env::var(key), Ok("VALUE".to_string()));s
 
     // creating jobs
-    let j1 = Arc::new(Job::Spacer(1u64));
-    // let j2 = Arc::new(Job::Spacer(1u64));
-    let j2 = Arc::new(Job::Lambda(Box::new(lambda_1)));
+    let j1 = Job::from_spacer(2u64);
+    let j2 = Job::from_lambda(lambda_1);
 
     // creating a routine_builder
-    let mut routine_builder = RoutineBuilder::new_with_capacity(2usize);
-    let vec = routine_builder.as_mut();
+    let mut routine_builder = RoutineBuilder::with_capacity(2usize);
     
     // adding jobs to the routine_builder
-    vec.push(j1);
-    vec.push(j2);
+    routine_builder.push(j1);
+    routine_builder.push(j2);
 
     // consuming and transforming routine_builder into routine
-    let routine = routine_builder.into();
+    let routine = routine_builder.build().unwrap();
 
     // building a component
-    let mut component_builder = ComponentBuilder::new().unwrap();
-    component_builder.set_name("custom");
-    component_builder.set_routine(routine);
-    component_builder.set_handler(handler);
+    let component_builder = ComponentBuilder::new(
+        "custom",
+        routine,
+        handler,
+    ).unwrap();
 
-    #[allow(unused)]
     let mut component = component_builder
         .build()
         .unwrap();
@@ -83,9 +84,10 @@ fn main() -> () {
         .unwrap()
         .block_on(async move {
             loop {
-                std::thread::sleep(std::time::Duration::from_secs(3u64));
+                std::thread::sleep(std::time::Duration::from_secs(1u64));
+                println!("sending message");
                 component.send(MS).unwrap();
-                component.send_run_request().unwrap();
+                // component.run_next_job().unwrap();
             }
         });
 }
