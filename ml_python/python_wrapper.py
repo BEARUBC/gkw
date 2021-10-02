dd30bc9ad9edb7be0d0fc44b75422818a7e27fb3
Zachary Chafe <zachary.chafe@outlook.com>
2021-10-02 15:33:47 -0700

    Starting python integration

---
+++ ml_python/python_wrapper.py
@@ -0,0 +1,10 @@
+# One thread to read stdin name: read_thread
+# One thread to read stdout name: out_thread
+# Data type queue or priority queue
+# Data structure will need a lock
+
+# Need access to stdin and stdout
+# Multithreading how to start a thread
+# Python locks
+# Data structures we can use
+# JSON string conversion
