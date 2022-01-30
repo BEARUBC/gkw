d26df859499a56c3a84dfcd20fa13e3af46ca979
Raunak Bhagat <rabhagat31@gmail.com>
2022-01-29 16:46:25 -0800

    Add gkw cli to help with repo initialization!

---
+++ .scripts/.hooks/pre-push.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+const { run, finish } = require('../utils');
+
+const { stdout } = command({
+  command: 'git',
+  args: ['rev-parse --abbrev-ref HEAD'],
+  output: false,
+});
+
+const branchName = stdout.toString().replace('\n', '');
+if (branch_name_policy.exec(branchName) === null) {
+  error('Invalid branch name; please follow the branch-naming policy: /(bug|task)/WAVY-[0-9]+/');
+  process.exit(1);
+}
+
+run({
+  title: 'Building...',
+  command: 'cargo',
+  args: ['build'],
+});
+
+run({
+  title: 'Running formatter...',
+  command: 'cargo',
+  args: ['+nightly fmt -- --check'],
+});
+
+run({
+  title: 'Running clips...',
+  command: 'cargo',
+  args: ['+nightly clippy'],
+});
+
+run({
+  title: 'Hello!',
+  command: 'ls',
+  args: ['-las'],
+});
+
+finish();
--- py_io/README.md
+++
@@ -1,7 +0,0 @@
-# Rust-Python Communications
-This directory contains:
-* input.json
-* output.json
-
-These files are used in communications with the Python analytics.
-They should not be tracked with VC because they should be used as buffers only.
