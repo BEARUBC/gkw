41dda1bce8cd945617fde3c0717798b71f0db35f
Zachary Chafe <zachary.chafe@outlook.com>
2022-06-25 13:28:24 -0700

    pulling main

--- application/kernel/src/main.rs
+++ application/kernel/src/main.rs
@@ -13,20 +13,20 @@ mod kernel;
 /// instantiated is 4.
 #[allow(unreachable_code)]
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
-async fn main() -> ! {
-    init::init_logging();
+// async fn main() -> ! {
+//     init::init_logging();
 
-    match init::init().await {
-        Ok(()) => info!("GKW initialization has succeeded."),
-        Err(err) => {
-            error!("GKW initialization has failed with error: {}.", err);
-            exit(1);
-        },
-    };
+//     match init::init().await {
+//         Ok(()) => info!("GKW initialization has succeeded."),
+//         Err(err) => {
+//             error!("GKW initialization has failed with error: {}.", err);
+//             exit(1);
+//         },
+//     };
 
-    #[allow(clippy::empty_loop)]
-    loop {}
+//     #[allow(clippy::empty_loop)]
+//     loop {}
 
-    error!("The GKW main-loop has crashed unexpectedly. Exiting...");
-    unreachable!()
-}
+//     error!("The GKW main-loop has crashed unexpectedly. Exiting...");
+//     unreachable!()
+// }
