5cc93b4bbe1be5ffeb60fb862522615a4de94b51
pjavanrood <Parshan.pjavanrood@gmail.com>
2022-11-26 15:32:28 -0800

    Starting to implement emg-parser

--- application/kernel/src/components/kernel/emg.rs
+++ application/kernel/src/components/kernel/emg.rs
@@ -5,6 +5,32 @@ pub(super) struct State;
 
 #[cfg(feature = "pseudo_analytics")]
 pub(super) fn parser(_: &mut State, _: f64) -> Result<()> {
+    let next_grip = GripType::from(data);
+
+    if(state.current_grip == next_grip) {
+        return Ok(());
+    }
+
+    match next_grip {
+        GripType::Cup => {
+            let channel = Channels::C_0;
+            
+        },
+
+        GripType::Hammer => {
+
+        },
+
+        GripType::Flat => {
+
+        }
+    }
+    
+
+
+    
+
+
     Ok(())
 }
 
