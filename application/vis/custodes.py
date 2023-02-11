8037a347599313ddc73c564bb297985089fd68f3
nathan-n-poon <55016299+nathan-n-poon@users.noreply.github.com>
2023-02-10 20:36:02 -0800

    matrix vis works now
    and added tester for the tester

---
+++ application/vis/custodes.py
@@ -0,0 +1,66 @@
+from tkinter import *
+import sys
+import json
+from random import randint
+root = Tk()
+
+
+lab = Label(root)
+lab.pack()
+
+canvas = Canvas()
+num_rows = 3
+num_cols = 3
+width = 50
+height = 50
+
+root.geometry(str(num_rows*width)+"x"+str(num_cols*height)+"+"+str(0)+"+"+str(0))
+
+def prop_to_hex(prop):
+    x = int(prop*255)
+    return '#{:02x}{:02x}{:02x}'.format(x, x, x)
+
+
+# class cell:
+#     def __init__(self):
+#         self.width = width
+#         self.height = height
+#         self.color = (0, 0, 0)
+#
+
+
+for row in range(num_rows):
+    for col in range(num_cols):
+        canvas.create_rectangle(row*width, col*height, row*width+width, col*height+height, fill=prop_to_hex(0))
+canvas.pack(fill=BOTH, expand=1)\
+
+def getMax(mat):
+    max = 0
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            if mat[i][j] > max:
+                max = mat[i][j]
+    return max
+
+def update():
+    mtext = sys.stdin.readline()
+
+    print('\n\n\n', mtext, '\n\n\n')
+    mat = json.loads(mtext)
+    print('\n\n\n', mat, '\n\n\n')
+    max = getMax(mat)
+
+    for row in range(num_rows):
+        for col in range(num_cols):
+            canvas.create_rectangle(row * width, col * height, row * width + width, col * height + height,
+                                    fill=prop_to_hex(mat[row][col]/max))
+    canvas.pack(fill=BOTH, expand=1)
+
+    root.after(500, update)
+
+
+
+# # run first time
+update()
+
+root.mainloop()
