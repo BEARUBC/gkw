d349da28b9237023e9e9fe8e284a061365a5877d
Ray Ho <108425957+Raywnh@users.noreply.github.com>
2023-02-18 14:37:09 -0800

    done fixing

--- application/emg_integration/python/testing.py
+++ application/emg_integration/python/testing.py
@@ -1,35 +1,24 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-import random
-import sys
-# Create figure for plotting
+import time
+
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 xs = []
 ys = []
-values = []
+x = 1
 
-# Grabs data from EMG stdout 
-
-# This function is called periodically from FuncAnimation
 def animate(i, xs, ys):
-
-    for line in sys.stdin:
-        if line.replace("\n","") != "":
-            values.append(float(line.replace("\n", "")))
-
     # Read voltage from EMG device
 
     # Add x and y to lists
     xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-    
-    if i == len(values):
-        sys.exit("No more data")
-        
-    ys.append(values[i])
-    i = i + 1
-        
+    global x
+    x = x / 2
+    i = x
+    print(x)
+    ys.append(i)
 
     # Limit x and y lists to 20 items
     xs = xs[-20:]
@@ -48,3 +37,4 @@ def animate(i, xs, ys):
 # Set up plot to call animate() function periodically
 ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=200)
 plt.show()
+
