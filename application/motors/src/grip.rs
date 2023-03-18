8a7f10408633a6d5677cfafbe8b879eff52f67b3
Nathan Poon <nathannpoon@gmail.com>
2023-03-18 15:30:05 -0700

    T

--- application/motors/src/grip.rs
+++ application/motors/src/grip.rs
@@ -1,5 +1,4 @@
-#[derive(PartialEq, Eq, Copy, Clone)]
-#[cfg_attr(not(release), derive(Debug))]
+#[derive(PartialEq, Copy, Clone)]
 pub enum Grip {
     Hammer{x: f64},
     Cup{x: f64},
@@ -9,24 +8,24 @@ pub enum Grip {
 impl From<Grip> for [u16; 3usize] {
     fn from(grip: Grip) -> [u16; 3usize] {
         match grip {
-            Grip::Hammer{x} => [4000*x, 4000*x, 4000*x],
-            Grip::Cup{x} => [5000*x, 5000*x, 5000*x],
-            Grip::Flat{x} => [6000*x, 6000*x, 6000*x],
+            Grip::Hammer{x} => [((4000 as f64)*x).floor() as u16, ((4000 as f64)*x).floor() as u16, ((4000 as f64)*x).floor() as u16],
+            Grip::Cup{x} => [((5000 as f64)*x).floor() as u16, ((5000 as f64)*x).floor() as u16, ((5000 as f64)*x).floor() as u16],
+            Grip::Flat{x} => [((6000 as f64)*x).floor() as u16, ((6000 as f64)*x).floor() as u16, ((6000 as f64)*x).floor() as u16],
         }
     }
 }
 
 impl Default for Grip {
     fn default() -> Self {
-        Self::Flat
+        Self::Flat{x: 1.0 as f64}
     }
 }
 
-impl From<f16> for Grip {
-    fn from(data: f16) -> Self {
+impl From<f64> for Grip {
+    fn from(data: f64) -> Self {
         const MODULO_BASE: u16 = 3;
-        let grip = (data.floor() as u64) % MODULO_BASE;
-        let scale = 2*(data - grip);
+        let grip = (data.floor() as u16) % MODULO_BASE;
+        let scale = 2.0*(data - grip as f64);
         match grip {
             0 => Self::Hammer{x: scale},
             1 => Self::Cup{x: scale},
--- application/motors/src/lib.rs
+++ application/motors/src/lib.rs
@@ -6,26 +6,25 @@ use std::io::Error as StdError;
 use raestro::maestro::builder::Builder;
 use raestro::maestro::constants::Baudrate;
 use raestro::maestro::constants::Channel;
-use raestro::maestro::constants::MAX_QTR_PWM;
-use raestro::maestro::constants::MIN_QTR_PWM;
 use raestro::maestro::Maestro;
-use std::sync::atomic::AtomicBool;
-use std::collections::Vec;
+use std::sync::{Arc, Mutex};
 use std::cmp::min;
 mod grip;
+use grip::*;
 
 
 pub struct Motor {
-    maestro: Maestro,
+    //maestro: Maestro,
     targetPosition: Arc<Mutex<f64>>,
     increment: u16,
-    channels: [Channel; 3usize]
+    channels: [Channel; 3usize],
+    runThread: JoinHandle<()>
 }
 
 
 impl Motor {
-    pub fn new(targetArc: Arc<Mutex<f64>>, incrementVal: u16) -> Result<Motor, StdError> {
-        let mut maestro: Maestro = Maestro = Builder::default()
+    pub fn new(incrementVal: u16) -> Result<Motor, StdError> {
+        let mut maestro: Maestro = Builder::default()
             .baudrate(Baudrate::Baudrate11520)
             .block_duration(Duration::from_millis(100))
             .try_into()
@@ -33,41 +32,68 @@ impl Motor {
         
         let channels = [Channel::Channel0, Channel::Channel1, Channel::Channel2];
 
+        let targetArc: Arc<Mutex<f64>> = Arc::new(Mutex::new(0.5 as f64));
+
+        let targetClone = (targetArc).clone();
+
         Ok(
             Motor {
-                maestro: maestro,
                 targetPosition: targetArc,
                 increment: incrementVal,
-                channels: channels
+                channels: channels,
+                runThread: thread::spawn(move || {
+                    loop {
+                        let grip: Grip = (*(targetClone.lock().unwrap())).into();
+                        let targets: [u16; 3usize] = grip.clone().into();
+
+                        for i in 0..3 {
+                            let channel = channels[i];
+                            let currentPosition = maestro.get_position(channel).unwrap();
+                            let targetPosition = targets[i];
+                            let nextPosition = min(targetPosition, 
+                                currentPosition + (((targetPosition - currentPosition) as f64).signum() as u16) * (incrementVal)
+                            );
+                            maestro.set_target(channel, nextPosition).unwrap();
+                        }
+                    }
+                })
             }
         )
     }
 
 
-    pub fn step(&mut self) -> Result<(), StdError>{
-        let grip: Grip = (self.targetPosition).lock().unwrap();
-        let targets: [u16; 3usize] = grip.clone().into();
+    pub fn cahngeGrip(&mut self, gripVal: f64) {
+        let mut targetGuard = self.targetPosition.lock().unwrap();
 
-        for i in 0..3 {
-            let mut channel = self.channels[i];
-            let currentPosition = maestro.get_position(channel).unwrap();
-            let targetPosition = targets[i];
-            let nextPosition = min(targetPosition, 
-                currentPosition +((targetPosition - currentPosition) as f64).signum() * self.increment
-            );
-            self.maestro.set_target(channel, nextPosition).unwrap();
-        }
-
-        Ok(())
+        *targetGuard = gripVal;
     }
 
-    pub fn run(&mut self) -> () {
-        let mut thread: JoinHandle<Result<(), StdError>> = thread::spawn(move || {
-            loop {
-                self.step().unwrap();
-            }
+    // pub fn step(&mut self) -> Result<(), StdError>{
+    //     let targetClone = (self.targetPosition).clone();
+    //     let grip: Grip = (*(targetClone.lock().unwrap())).into();
+    //     let targets: [u16; 3usize] = grip.clone().into();
 
-            Err(StdError::new(ErrorKind::Other, "MOTOR ERROR"));
-        });
-    }
+    //     for i in 0..3 {
+    //         let mut channel = self.channels[i];
+    //         let currentPosition = self.maestro.get_position(channel).unwrap();
+    //         let targetPosition = targets[i];
+    //         let nextPosition = min(targetPosition, 
+    //             currentPosition + (((targetPosition - currentPosition) as f64).signum() as u16) * (self.increment)
+    //         );
+    //         self.maestro.set_target(channel, nextPosition).unwrap();
+    //     }
+
+    //     Ok(())
+    // }
+
+    // pub fn run(&mut self) -> () {
+    //     let mut thread: JoinHandle<()> = thread::spawn(move || {
+    //         loop {
+    //             match self.step() {
+    //                 Ok(()) => (),
+    //                 Err(err) => break,
+    //             }
+    //         }
+    //     });
+    // }
 }
