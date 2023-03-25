70b763d2115d043338b5d19c3a746571b0ed0e6d
AsonI3210 <asoniubc@gmail.com>
2023-03-25 14:24:33 -0700

    UNTESTED script for oakd host communication

--- application/Oak-DTest/test.py
+++ application/Oak-DTest/test.py
@@ -2,13 +2,13 @@
 import depthai as dai
 import json
 
-pipeline = dai.Pipeline()
+pipeline = dai.Pipeline() # Creates pipeline 
 
-xin = pipeline.create(dai.node.XLinkIn)
-xin.setStreamName('in')
+xin = pipeline.create(dai.node.XLinkIn) 
+xin.setStreamName('in') #host - device communication 
 
 script = pipeline.create(dai.node.Script)
-xin.out.link(script.inputs['in'])
+xin.out.link(script.inputs['in']) #Device to host, multiple inputs???
 script.setScript("""
     import json
 
@@ -26,7 +26,7 @@ script.setScript("""
     node.io['out'].send(b)
 """)
 
-xout = pipeline.create(dai.node.XLinkOut)
+xout = pipeline.create(dai.node.XLinkOut)#Okd --> device 
 xout.setStreamName('out')
 script.outputs['out'].link(xout.input)
 
@@ -34,15 +34,16 @@ script.outputs['out'].link(xout.input)
 with dai.Device(pipeline) as device:
     # This dict will be serialized (JSON), sent to device (Script node),
     # edited a bit and sent back to the host
+
     dict = {'one':1, 'foo': 'bar'}
     print('dict', dict)
     data = json.dumps(dict).encode('utf-8')
-    buffer = dai.Buffer()
-    buffer.setData(list(data))
-    device.getInputQueue("in").send(buffer)
+    buffer = dai.Buffer() 
+    buffer.setData(list(data)) # Buffering communication from host to device 
+    device.getInputQueue("in").send(buffer) # From "in" link, send data to buffer
 
     # Wait for the script to send the changed dictionary back
-    jsonData = device.getOutputQueue("out").get()
+    jsonData = device.getOutputQueue("out").get() # Once available recieve data 
     jsonText = str(jsonData.getData(), 'utf-8')
     changedDict = json.loads(jsonText)
     print('changedDict', changedDict)
